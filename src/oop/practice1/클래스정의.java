dac3952823eed46617d5a3270cff927261c55de2
10cheon00 <10cheon00@gmail.com>
2024-09-22 22:37:28 +0900

    fix: 클래스 정의 문제 수정

--- src/oop/practice1/클래스정의.java
+++ src/oop/practice1/클래스정의.java
@@ -6,7 +6,8 @@ package oop.practice1;
  */
 public class 클래스정의 {
     public static void main(String[] args) {
-        // todo: '클래스정의.java'파일과 같은 폴더에 'YourFirstClass' 라는 이름을 가진 클래스를 새로 만들어보고, 이 파일로 import 하세요!
+        // todo: '클래스정의.java'파일과 같은 폴더에 'YourFirstClass' 라는 이름을 가진 클래스를 새로 만들어보고,
+        //       YourFirstClass 파일을 이 곳으로 import 하세요!
         //      컴파일이 되어야 하고, 아래 코드를 실행했을 때 클래스 이름이 결과물로 출력되어야 합니다!
 
         System.out.println(YourFirstClass.class.getName());
