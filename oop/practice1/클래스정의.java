5241145c6473b539d77261b4b932fcf300ac082d
10cheon00 <10cheon00@gmail.com>
2024-09-22 17:28:25 +0900

    feat: 클래스 생성 문제

---
+++ oop/practice1/클래스정의.java
@@ -0,0 +1,7 @@
+package oop.practice1;
+
+public class 클래스정의 {
+    public static void main(String[] args) {
+        // todo: '클래스정의.java'파일과 같은 폴더에 아무 이름을 가진 클래스를 새로 만들어보고, 이 곳에 import 하기.
+    }
+}
