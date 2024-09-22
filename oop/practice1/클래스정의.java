297449553119aee6ef4024a628c86ae6ff451d3c
10cheon00 <10cheon00@gmail.com>
2024-09-22 17:48:18 +0900

    fix: 클래스정의 설명 추가

--- oop/practice1/클래스정의.java
+++ oop/practice1/클래스정의.java
@@ -1,7 +1,14 @@
 package oop.practice1;
 
+/**
+ * 이 파일은 문제 제시 및 테스트용 클래스로 절대 수정하지 않습니다.
+ * 여러분들은 todo를 따라 문제를 해결하시면 됩니다!
+ */
 public class 클래스정의 {
     public static void main(String[] args) {
-        // todo: '클래스정의.java'파일과 같은 폴더에 아무 이름을 가진 클래스를 새로 만들어보고, 이 곳에 import 하기.
+        // todo: '클래스정의.java'파일과 같은 폴더에 'YourFirstClass' 라는 이름을 가진 클래스를 새로 만들어보고, 이 파일로 import 하세요!
+        //      컴파일이 되어야 하고, 아래 코드를 실행했을 때 클래스 이름이 결과물로 출력되어야 합니다!
+
+        System.out.println(YourFirstClass.class.getName());
     }
 }
